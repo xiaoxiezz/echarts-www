867d49e19e10bb343fba02927c11712d1314cef7
iMaeGoo <mail1st@qq.com>
2021-11-23 19:47:50 +0800

    Update build.js

--- bin/build.js
+++ bin/build.js
@@ -215,18 +215,6 @@ async function buildJade(config) {
             cfg.pageConfig = {};
         }
 
-        // This props can be read in jade tpl, like: `#{cdnPayRoot}`
-        assert(
-            cfg.cdnPayRoot
-            && cfg.cdnFreeRoot
-            && cfg.host
-            && cfg.cdnThirdParty
-            && cfg.galleryPath
-            && cfg.releaseDestDir
-            && cfg.homeVersion
-            && cfg.cdnPayVersion
-        );
-
         return cfg;
     }
 
