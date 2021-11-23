c66dae87248adfa55a6d8b93c87f47200caf4763
iMaeGoo <mail1st@qq.com>
2021-11-23 19:16:20 +0800

    Update env.asf.js

--- config/env.asf.js
+++ config/env.asf.js
@@ -6,14 +6,18 @@ config.host = 'https://echarts.apache.org';
 config.cdnPayRootMap = {
     // Expensive!!! use it carefully.
     // zh: 'https://echarts-www.cdn.bcebos.com', // origin: 'https://echarts-www.bj.bcebos.com'
-    zh: 'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site',
-    en: 'https://echarts.apache.org'
+    // zh: 'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site',
+    // en: 'https://echarts.apache.org'
+    zh: '',
+    en: ''
 };
 config.cdnFreeRootMap = {
     // 'echarts.cdn.apache.org' have been configured for zh (?)
     // zh: 'https://echarts.cdn.apache.org',
-    zh: 'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site',
-    en: 'https://echarts.apache.org'
+    // zh: 'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site',
+    // en: 'https://echarts.apache.org'
+    zh: '',
+    en: ''
 };
 config.galleryPath = 'https://echarts.apache.org/examples/';
 config.releaseDestDir = path.resolve(__dirname, '../../echarts-website');
