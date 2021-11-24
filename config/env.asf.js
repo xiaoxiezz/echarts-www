aa9c7a37b0ea9dcb5265e0f8a9e9eb7300ad129b
iMaeGoo <mail1st@qq.com>
2021-11-24 10:30:41 +0800

    Update env.asf.js

--- config/env.asf.js
+++ config/env.asf.js
@@ -1,25 +1,25 @@
 const path = require('path');
 const config = require('./common');
 
-config.host = '';
+config.host = '/echarts4';
 
 config.cdnPayRootMap = {
     // Expensive!!! use it carefully.
     // zh: 'https://echarts-www.cdn.bcebos.com', // origin: 'https://echarts-www.bj.bcebos.com'
     // zh: 'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site',
     // en: 'https://echarts.apache.org'
-    zh: '',
-    en: ''
+    zh: '/echarts4',
+    en: '/echarts4'
 };
 config.cdnFreeRootMap = {
     // 'echarts.cdn.apache.org' have been configured for zh (?)
     // zh: 'https://echarts.cdn.apache.org',
     // zh: 'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site',
     // en: 'https://echarts.apache.org'
-    zh: '',
-    en: ''
+    zh: '/echarts4',
+    en: '/echarts4'
 };
-config.galleryPath = '/examples/';
+config.galleryPath = '/echarts4/examples/';
 config.releaseDestDir = path.resolve(__dirname, '../../echarts-website');
 
 module.exports = config;
